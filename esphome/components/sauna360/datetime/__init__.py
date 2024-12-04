0834f3889de495e3ccb88636aac6a34765614e6b
MrMstr <34895525+tesu84@users.noreply.github.com>
2024-12-04 19:30:22 +0200

    Fixed datetime init, thanks @swoboda1337

--- esphome/components/sauna360/datetime/__init__.py
+++ esphome/components/sauna360/datetime/__init__.py
@@ -3,7 +3,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import datetime
 from esphome.const import (
-    CONF_ID,
     CONF_ICON,
 )
 
@@ -17,49 +16,36 @@ SAUNA360DateTime = sauna360_ns.class_("SAUNA360DateTime", datetime.DateTimeEntit
 SAUNA360NotAllowedStartFromTime = sauna360_ns.class_("SAUNA360NotAllowedStartFromTime", datetime.TimeEntity, cg.PollingComponent)
 SAUNA360NotAllowedStartUntilTime = sauna360_ns.class_("SAUNA360NotAllowedStartUntilTime", datetime.TimeEntity, cg.PollingComponent)
 
-CONF_DATE_TIME = "date_time"
 CONF_NOT_ALLOWED_START_FROM = "not_allowed_start_from"
 CONF_NOT_ALLOWED_START_UNTIL = "not_allowed_start_until"
 
-CONFIG_SCHEMA = cv.All(
-    cv.COMPONENT_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(SAUNA360DateTime),
-            cv.GenerateID(CONF_SAUNA360_ID): cv.use_id(SAUNA360Component),
-            cv.Optional(CONF_DATE_TIME): datetime.datetime_schema(
-                SAUNA360DateTime,
-            ).extend(
-                {
-                    cv.Optional(CONF_ICON, default="mdi:calendar-clock"): cv.icon,
-                }
-            ),
-            cv.Optional(CONF_NOT_ALLOWED_START_FROM): datetime.time_schema(
-                SAUNA360NotAllowedStartFromTime,
-            ).extend(
-                {
-                    cv.Optional(CONF_ICON, default="mdi:timelapse"): cv.icon,
-                }
-            ),
-            cv.Optional(CONF_NOT_ALLOWED_START_UNTIL): datetime.time_schema(
-                SAUNA360NotAllowedStartUntilTime,
-            ).extend(
-                {
-                    cv.Optional(CONF_ICON, default="mdi:timelapse"): cv.icon,
-                }
-            ),
-        }
-    ),
+CONFIG_SCHEMA = datetime.datetime_schema(SAUNA360DateTime).extend(
+    {
+        cv.GenerateID(CONF_SAUNA360_ID): cv.use_id(SAUNA360Component),
+        cv.Optional(CONF_NOT_ALLOWED_START_FROM): datetime.time_schema(
+            SAUNA360NotAllowedStartFromTime,
+        ).extend(
+            {
+                cv.Optional(CONF_ICON, default="mdi:timelapse"): cv.icon,
+            }
+        ),
+        cv.Optional(CONF_NOT_ALLOWED_START_UNTIL): datetime.time_schema(
+            SAUNA360NotAllowedStartUntilTime,
+        ).extend(
+            {
+                cv.Optional(CONF_ICON, default="mdi:timelapse"): cv.icon,
+            }
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA).extend(
+    {
+        cv.Optional(CONF_ICON, default="mdi:calendar-clock"): cv.icon,
+    }
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await datetime.new_datetime(config)
     await cg.register_component(var, config)
-    if CONF_DATE_TIME in config:
-        datetime_struct = cg.StructInitializer(
-            cg.ESPTime,
-        )
-        datetime_var = await datetime.new_datetime(config[CONF_DATE_TIME])
-        cg.add(datetime_var.set_initial_value(datetime_struct))
     if CONF_NOT_ALLOWED_START_FROM in config:
         time_struct = cg.StructInitializer(
             cg.ESPTime,
